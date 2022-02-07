abba240a306c9f27d6190d2dfa8fd5724f105b44
Carlo Marcello Menjivar Montes de Oca <marcello_menjivar@unitec.edu>
2022-02-07 14:06:00 -0600

    nueva clase de cadenas

---
+++ cadenas_CarloMenjivar20551123/src/cadenas_carlomenjivar20551123/Cadenas_CarloMenjivar20551123.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadenas_carlomenjivar20551123;
+import java.util.Scanner;
+
+/**
+ *
+ * @author megam
+ */
+public class Cadenas_CarloMenjivar20551123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        String palabra;
+        
+        System.out.print("Ingrese una Palabra: ");
+        palabra = entrada.next();
+        
+        palabra = palabra.toUpperCase();
+        
+        System.out.println(palabra);
+        
+        
+    }
+    
+}
