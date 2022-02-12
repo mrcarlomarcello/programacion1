56e9d80b79fbf7ae0f11447bbf2305b38fcbc5a1
Carlo Marcello Menjivar Montes de Oca <marcello_menjivar@unitec.edu>
2022-02-11 21:52:46 -0600

    agregando la prueba S4

---
+++ S4_ciclofor_figuras_CarloMenjivar20551123/src/s4_ciclofor_figuras_carlomenjivar20551123/S4_ciclofor_figuras_CarloMenjivar20551123.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package s4_ciclofor_figuras_carlomenjivar20551123;
+
+/**
+ *
+ * @author megam
+ */
+public class S4_ciclofor_figuras_CarloMenjivar20551123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
