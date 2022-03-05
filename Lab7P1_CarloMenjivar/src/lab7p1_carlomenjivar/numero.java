228ea565d624661fdd5be8f62e11a4043aac163c
shello <marcello_menjivar@unitec.edu>
2022-03-04 18:41:40 -0600

    El laboratorio 7 está funcional, el ejercicio está raro...todavia parte 2

--- Lab7P1_CarloMenjivar/src/lab7p1_carlomenjivar/numero.java
+++ Lab7P1_CarloMenjivar/src/lab7p1_carlomenjivar/numero.java
@@ -5,8 +5,7 @@ package lab7p1_carlomenjivar;
 public class numero {
     
     public int [] temp;
-    public miscClass misc = new miscClass();
-    
+    public miscClass misc = new miscClass();    
     //constructor
     public void numero(int size){        
         this.temp = misc.LlenarEnteros(size, 25);
@@ -19,18 +18,22 @@ public class numero {
         
         int [] primos = new int[this.temp.length];
         int contadorprimos;
+        int number=0;
         
         for(int i =0; i< this.temp.length; i++){
             
             contadorprimos=0;
             
             for(int j = 2; j< this.temp[i]; j++) {
-                int number = this.temp[i];
-                while(number%j == 0) {
-                    contadorprimos++;
+                number = this.temp[i];
+                while(number%j == 0) {                    
                     number = number/j;
                 }                
-            }
+            }         
+            if(number >2) {
+                contadorprimos++;
+            }           
+                        
             if(contadorprimos==0){
                 primos[i]=-1;
             }else{
