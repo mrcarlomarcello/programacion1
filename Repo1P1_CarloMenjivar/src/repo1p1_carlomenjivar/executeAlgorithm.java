8783d3d45231db9bb8e54e53fa9d228c58aa3136
shello <marcello_menjivar@unitec.edu>
2022-03-06 15:39:33 -0600

    Cambiandome a la mac para estar chilling... pt2

--- Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/executeAlgorithm.java
+++ Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/executeAlgorithm.java
@@ -3,9 +3,23 @@ package repo1p1_carlomenjivar;
 
 public class executeAlgorithm {
     
-    //Algoritmo del ejercicio 1
+    private final miscClass misc = new miscClass();
+    private final mate mate = new mate();
+    //Algoritmo del ejercicio 1 pie
     public void ejercicio1(){
-        System.out.println("Ejercicio 1");
+        System.out.println("Ejercicio 1 - escriba un limite:");
+        int M = misc.leerEntero();
+        double pie = 0;
+        double top;
+        double down;
+        double result;
+        for(int i = 0; i<= M; i++){
+            top = mate.exponencial((-1), i);
+            down = ((2*i)+1);
+            result =top/down;
+            pie += result;
+        }
+        System.out.println(pie);        
     }
     //Algoritmo del ejercicio 2
     public void ejercicio2(){
---
+++ Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/mate.java
@@ -0,0 +1,13 @@
+
+package repo1p1_carlomenjivar;
+
+
+public class mate {
+    
+    public double exponencial(double base, int exponencial){           
+            double power = 1;                    
+            for(int i = 1; i <=exponencial; i++){ power = power*base;}
+            return power;
+    }
+        
+}
