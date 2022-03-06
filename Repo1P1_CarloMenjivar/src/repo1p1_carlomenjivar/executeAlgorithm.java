7f122493597dcd1d262dd65dfc74df2f132d51a1
Marcello Menjivar Montes De Oca <marcello_menjivar@unitec.edu>
2022-03-06 17:36:18 -0600

    Agregue el ejercicio 2 de la repo

--- Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/executeAlgorithm.java
+++ Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/executeAlgorithm.java
@@ -5,6 +5,7 @@ public class executeAlgorithm {
     
     private final miscClass misc = new miscClass();
     private final mate mate = new mate();
+    
     //Algoritmo del ejercicio 1 pie
     public void ejercicio1(){
         System.out.println("Ejercicio 1 - escriba un limite:");
@@ -16,17 +17,41 @@ public class executeAlgorithm {
         for(int i = 0; i<= M; i++){
             top = mate.exponencial((-1), i);
             down = ((2*i)+1);
-            result =top/down;
-            pie += result;
+            result = top/(double)down;
+            pie = pie + result;
         }
         System.out.println(pie);        
     }
+    
     //Algoritmo del ejercicio 2
     public void ejercicio2(){
-        System.out.println("Ejercicio 2");
+        System.out.println("Ejercicio 2 - Ingresa un numero igual o mayor a 7");
+        
+        int border = 0;
+        while(border<7){
+            border = misc.leerEntero();
+        }
+        
+        mate.sandwich(border);
+        
     }
+    
     //Algoritmo del ejercicio 3
     public void ejercicio3(){
-        System.out.println("Ejercicio 3"); 
+        System.out.println("Ejercicio 3 -  Ingresa una cadena con comandos -w y -p");
+        String comando = misc.leerCadena();
+        
+        for(int i =0; i < (comando.length()-1); i++){
+            if(comando.substring(i, i+2).equals("-p")){
+                System.out.println("Got it");
+            }
+        }
+        
+        for(int i =0; i < (comando.length()-1); i++){
+            if(comando.substring(i, i+2).equals("-w")){
+                System.out.println("Got it");
+            }
+        }
     }
+    
 }
--- Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/mate.java
+++ Repo1P1_CarloMenjivar/src/repo1p1_carlomenjivar/mate.java
@@ -9,5 +9,31 @@ public class mate {
             for(int i = 1; i <=exponencial; i++){ power = power*base;}
             return power;
     }
+    
+    public void sandwich(int limit){
+        String [][] sandwich = new String[limit][limit];
         
+        for(int i = 0 ; i< limit; i++){
+            for(int j=0; j < limit; j++){
+                if(i==j){
+                    sandwich[i][j] = "   ";
+                }else if(i == 0 || j == 0 || i == (limit-1) || j == (limit-1)){
+                    sandwich[i][j] = " * ";   
+                }else if((j-1)==i){
+                    sandwich[i][j] = " * ";
+                }else if((j+1)==i){
+                    sandwich[i][j] = " * ";
+                }else {
+                    sandwich[i][j] = " + ";   
+                }                
+            }
+        }
+        
+        for(int i = 0 ; i< limit; i++){
+            for(int j=0; j < limit; j++){
+                System.out.print(sandwich[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
